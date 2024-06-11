6373aec12f6433c1d75e6ea8f92f439934ff9643
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-06-11 11:07:46 -0700

    first commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var name = "Jonah";
+var date = DateTime.Now; 
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Hello {name}, the current date and time is {date}");
+Console.WriteLine("There are 198 days until the next Christmas.");
