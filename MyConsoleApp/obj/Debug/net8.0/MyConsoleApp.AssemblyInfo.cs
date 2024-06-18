2013e0843ea650d1abbba2e03d36e1918daec691
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-06-18 10:30:05 -0700

    Azure Commit

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6373aec12f6433c1d75e6ea8f92f439934ff9643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fa2e750e4a301a6302994987d0e5f515f71c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
